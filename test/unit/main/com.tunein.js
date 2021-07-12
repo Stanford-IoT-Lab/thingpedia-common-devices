9f08077a5f24b4a11600c5cb9b00300c62c2f76e
Jake Wu <jmhw0123@gmail.com>
2021-07-13 02:47:35 +0800

    fixed http parsing bugs and updated test cases

--- test/unit/main/com.tunein.js
+++ test/unit/main/com.tunein.js
@@ -17,19 +17,31 @@ module.exports = [
         ]
     }, (results) => {
         assert(results instanceof Array);
-        assert.deepStrictEqual(results[0].id, new Tp.Value.Entity('tunein:station:s25876', 'ESPN Radio'));
+        assert.deepStrictEqual(results[0].id, new Tp.Value.Entity('station:s25876', 'ESPN Radio'));
         assert(results[0].show instanceof Tp.Value.Entity);
-        assert(results[0].link instanceof Tp.Value.Entity);
-        assert(results[0].link, 'http://opml.radiotime.com/Tune.ashx?id=s25876');
-        assert.deepStrictEqual(results[0].image, Tp.Value.Entity('tt:picture', 'http://cdn-profiles.tunein.com/s25876/images/logoq.jpg?t=1'));
+        assert(typeof results[0].link === 'string');
+        assert(results[0].link === 'http://opml.radiotime.com/Tune.ashx?id=s25876');
+        assert(results[0].image === 'http://cdn-profiles.tunein.com/s25876/images/logoq.jpg?t=1');
+    }],
+    ['query', 'station', {}, {
+        filter: [
+            ['id', '=~', 'espn chicago']
+        ]
+    }, (results) => {
+        assert(results instanceof Array);
+        assert.deepStrictEqual(results[0].id, new Tp.Value.Entity('station:s21297', 'ESPN Chicago'));
+        assert(results[0].show instanceof Tp.Value.Entity);
+        assert(typeof results[0].link === 'string');
+        assert(results[0].link === 'http://opml.radiotime.com/Tune.ashx?id=s21297');
+        assert(results[0].image === 'http://cdn-profiles.tunein.com/s21297/images/logoq.jpg');
     }],
     ['query', 'most_popular_stations', {}, (results) => {
         assert(results instanceof Array);
         for (const item of results) {
             assert(item.id instanceof Tp.Value.Entity);
             assert(item.show instanceof Tp.Value.Entity);
-            assert(item.link instanceof Tp.Value.Entity);
-            assert(item.image instanceof Tp.Value.Entity);
+            assert(typeof item.link === 'string');
+            assert(typeof item.image === 'string');
             assert(item.link.startsWith('http://'), `Expected link to start with http://, got ${item.link}`);
             assert(item.image.startsWith('http://'), `Expected image to start with http://, got ${item.image}`);
         }
@@ -39,8 +51,8 @@ module.exports = [
         for (const item of results) {
             assert(item.id instanceof Tp.Value.Entity);
             assert(item.show instanceof Tp.Value.Entity);
-            assert(item.link instanceof Tp.Value.Entity);
-            assert(item.image instanceof Tp.Value.Entity);
+            assert(typeof item.link === 'string');
+            assert(typeof item.image === 'string');
             assert(item.link.startsWith('http://'), `Expected link to start with http://, got ${item.link}`);
             assert(item.image.startsWith('http://'), `Expected image to start with http://, got ${item.image}`);
         }
